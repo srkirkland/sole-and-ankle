93e07fe586b149570b9f9410c2e612936363482f
Scott Kirkland <srkirkland@gmail.com>
2022-10-21 09:56:21 -0700

    e1: fix superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,15 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  padding: 8px 35px;
+  gap: 24px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  flex: 1;
+  margin-left: 32px;
 `;
 
 const HelpLink = styled.a`
